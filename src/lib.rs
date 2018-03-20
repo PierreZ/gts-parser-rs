0501f0fa952aba7e866c9ac1c7bd314c351dd0b8
Pierre Zemb <contact@pierrezemb.fr>
2018-03-20 22:59:40 +0100

    feat: moving to warp10 lib from clevercloud

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,38 @@
 #[macro_use]
 extern crate nom;
+extern crate time;
+extern crate warp10;
 
 use nom::digit;
 use std::str;
 
-#[derive(Debug,PartialEq)]
-// TODO: move to https://github.com/CleverCloud/warp10.rs/blob/master/src/data.rs#L95
-pub struct GTS {
-    pub timestamp: i32,
-    pub classname: String,
-    pub labels: String,
-}
-
-named!(parse_gts<&str, GTS>,
+named!(parse_gts<&str, warp10::Data>,
     do_parse!(
-        timestamp: digit >>
-        tag!("// ")   >>
-        classname: take_until!("{") >> 
+        // TODO: put actual timestamp and test it somehow
+        timestamp: alt!( digit | value!("1")) >>
+        tag!("/")   >>
+        // TODO: handle geo
+        // /48.0:-4.5/
+        tag!("/ ")   >>
+        classname: take_until!("{") >>
         tag!("{") >>
+        // TODO: handle labels
+        // label0=val0,label1=val1 into Vec
         labels: take_until!("}") >>
-        tag!("}") >> 
-        tag!(" ") >> 
-        (GTS {
-            timestamp: timestamp.to_string().parse::<i32>().unwrap(),
-            classname: classname.to_string(),
-            labels: labels.to_string()
-        })
+        tag!("}") >>
+        tag!(" ") >>
+        value: take_until_and_consume!("\n") >>
+        (warp10::Data::new(
+                time::Timespec::new(0, timestamp.to_string().parse::<i32>().unwrap() * 1000),
+                None,
+                classname.to_string(),
+                Vec::new(),
+                warp10::Value::Int(value.to_string().parse::<i32>().unwrap()),
+        ))
     )
 );
 
 #[test]
 fn parse() {
-    assert_eq!(parse_gts("21344// test{label0=val0} 42"),
-               Ok(("42",
-                   GTS {
-                       timestamp: 21344,
-                       classname: "test".to_string(),
-                       labels: "label0=val0".to_string(),
-                   })));
+    println!("{:?}", parse_gts("21344// test{label0=val0} 42\n"))
 }
